9b4af644a2b71eae8c3efeb2f589aff36b4fdd2f
Martin Wecke <>
2020-04-14 08:39:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+`````
+        __
+       /\  \    
+      |::\  \   
+      |:|:\  \  
+    __|:|\:\  \ 
+   /::::|_\:\__\
+   \:\~~\  \/__/
+    \:\  \      
+     \:\  \     
+      \:\__\    
+       \/__/    
+
+`````
+
+Tiny ES6 module template for pregressive enhancement.
+
+### Installation
+
+`npm install @superstructure.net/m`
+
+
+### Usage
+
+`````
+import M from @superstructure.net/m;
+
+export default class MyModule extends M {
+  constructor() {
+    super({
+      namespace: 'myModule',
+      mediaQuery: '( min-width: 800px )'
+    });
+  }
+
+  onInit() {}
+
+  onResize() {}
+
+  onDestroy() {}
+}
+`````
+
+### Features
+
++ Automatically init / destroy modules depending on media queries.
++ Add namespaced delegated events via `this.addEvent()` that get automatically removed when the module is destroyed.
+
+### Lifecycle Functions
+
+##### `onInit()`
+
+##### `onResize()`
+
+##### `onDestroy()`
+
+##### `onInit()`
+
+### Methods
+
+##### `this.addEvent( type, selector, handler, options )`
+Add delegated namespaced events that get automatically removed when the module s destroyed.
+`type` — `click`, `touchstart`, `module/action`
+`selector` — `.class`, `[data-role="element"]`, `window`, `null = document`
+`handler` — `function( event )`
+`options` — options passed to addEventListener like `{ capture: true }` 
+
+##### `this.triggerEvent( type, data, element )`
+Trigger `type` event on `element` (defaults to `document`) passing an optional `data` object.
+
+
+##### `this.selector( role )`
+Returns an attribute selector based on the modules namespace and the passed role. 
+`this.selector( 'content' )` returns `[data-{namespace}-role="content"]`
+
+##### `destroy()`
+Manually destroy the module instance.
+
+
+
+
+
+
---
+++ m.js
@@ -0,0 +1,157 @@
+/**
+ * 
+        __
+       /\  \    
+      |::\  \   
+      |:|:\  \  
+    __|:|\:\  \ 
+   /::::|_\:\__\
+   \:\~~\  \/__/
+    \:\  \      
+     \:\  \     
+      \:\__\    
+       \/__/    
+
+M
+
+Tiny ES6 module template for progressive enhancement.
+ * 
+ */
+
+export default class M {
+  constructor(options) {
+    this._namespace = options.namespace;
+    this._mediaQuery = options.mediaQuery;
+    this._events = [];
+    this._resizeTimer = null;
+    this._initiated = false;
+
+    this._bindEvents();
+    this._checkMediaQuery();
+  }
+
+  /**
+   * Exposed functions
+   */
+  addEvent(type, selector = document, handler = () => {}, options = {}) {
+    if (!type) {
+      return;
+    }
+
+    let _event = {};
+    _event['type'] = type;
+    _event['target'] = selector === window ? window : document;
+    _event['options'] = options;
+    _event['handler'] = () => {
+      // if no selector or selector is root element,
+      // directly call handler
+      // otherwise check for delegation
+      if (!selector || selector === document || selector === window) {
+        handler.call(this, event);
+      } else if (event.target === document) {
+        // ignoring events bubbled to the root...
+      } else {
+        if (
+          event.target.matches(selector) ||
+          (!options.capture && event.target.closest(selector))
+        ) {
+          handler.call(this, event);
+        }
+      }
+    };
+    _event['handler'].bind(this);
+
+    _event['target'].addEventListener(type, _event['handler'], options);
+
+    this._events.push(_event);
+  }
+
+  triggerEvent(type, data = {}, element = document) {
+    if (!type) {
+      return;
+    }
+
+    let _event = new CustomEvent(type, {
+      bubbles: true,
+      detail: data
+    });
+
+    element.dispatchEvent(_event);
+  }
+
+  selector(role) {
+    if (!role) {
+      return;
+    }
+
+    return '[data-' + this._namespace + '-role="' + role + '"]';
+  }
+
+  /**
+   * Internal functions
+   */
+  _init() {
+    if (this.onInit) {
+      this.onInit();
+    }
+
+    this._initiated = true;
+    document.documentElement.classList.add('initiated--' + this._namespace);
+  }
+
+  destroy() {
+    this._removeEvents();
+
+    document.documentElement.classList.remove('initiated--' + this._namespace);
+    this._initiated = false;
+
+    if (this.onDestroy) {
+      this.onDestroy();
+    }
+  }
+
+  _bindEvents() {
+    window.addEventListener(
+      'resize',
+      (this._handleResize = this._handleResize.bind(this))
+    );
+  }
+
+  _removeEvents() {
+    this._events.forEach(event => {
+      event['target'].removeEventListener(
+        event['type'],
+        event['handler'],
+        event['options']
+      );
+    });
+
+    this._events = [];
+  }
+
+  _handleResize() {
+    if (this._resizeTimer) {
+      clearTimeout(this._resizeTimer);
+    }
+
+    this._resizeTimer = setTimeout(() => {
+      this._onResize();
+    }, 100);
+  }
+
+  _onResize() {
+    this._checkMediaQuery();
+  }
+
+  _checkMediaQuery() {
+    if (window.matchMedia(this._mediaQuery).matches) {
+      if (!this._initiated) {
+        this._init();
+      }
+    } else {
+      if (this._initiated) {
+        this.destroy();
+      }
+    }
+  }
+}
