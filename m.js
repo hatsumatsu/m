2ac78f969d55ca026c8158c0a7361bd5e5e46aa9
Martin Wecke <mail@hatsumatsu.de>
2022-08-19 14:08:24 +0200

    base mount/unmount logic on MediaQueryList change event + avoid onResize hook from firing multiple times

--- m.js
+++ m.js
@@ -37,100 +37,128 @@ export default class MyModule extends M {
  */
 
 export default class M {
-    constructor(mediaQuery = 'screen') {
-        this._name = this.constructor.name.toLowerCase();
-        this._mediaQuery = mediaQuery;
-        this._resizeTimer = null;
-        this._viewport = {
-            prevWidth: undefined,
-            prevHeight: undefined,
-            width: undefined,
-            height: undefined,
-        };
+  constructor(mediaQuery = 'screen') {
+    this._name = this.constructor.name.toLowerCase();
 
-        this._initiated = false;
+    this._mediaQuery = mediaQuery;
+    this._mediaQueryList = window.matchMedia(mediaQuery);
 
-        this._bindEvents();
-        this._onResize(false);
+    this._resizeDelay = 500;
+    this._resizeDebounce = null;
+    this._runResizeHook = false;
+    this._viewport = {
+      prevWidth: undefined,
+      prevHeight: undefined,
+      width: undefined,
+      height: undefined
+    };
+
+    this._initiated = false;
+
+    this._onResize();
+    this._onMediaQueryChange();
+
+    this._bindEvents();
+  }
+
+  selector(role) {
+    if (!role) {
+      return;
     }
 
-    selector(role) {
-        if (!role) {
-            return;
-        }
+    return '[data-' + this._name + '-role~="' + role + '"]';
+  }
 
-        return '[data-' + this._name + '-role~="' + role + '"]';
+  /**
+   * Internal functions
+   */
+  _init() {
+    this._initiated = true;
+    document.documentElement.classList.add('initiated--' + this._name);
+
+    if (this.onInit) {
+      this.onInit();
+    }
+  }
+
+  _destroy() {
+    if (this.onDestroy) {
+      this.onDestroy();
+    }
+
+    document.documentElement.classList.remove('initiated--' + this._name);
+    this._initiated = false;
+  }
+
+  _bindEvents() {
+    /**
+     * MediaQueryList listener
+     */
+    if (this._mediaQueryList.addEventListener) {
+      this._mediaQueryList.addEventListener('change', () => {
+        this._handleMediaQueryChange();
+      });
+    } else if (this._mediaQueryList.addListener) {
+      this._mediaQueryList.addListener(() => {
+        this._handleMediaQueryChange();
+      });
     }
 
     /**
-     * Internal functions
+     * Resize event
      */
-    _init() {
-        this._initiated = true;
-        document.documentElement.classList.add('initiated--' + this._name);
+    window.addEventListener('resize', () => {
+      this._handleResize();
+    });
+  }
 
-        if (this.onInit) {
-            this.onInit();
-        }
+  _handleMediaQueryChange() {
+    this._runResizeHook = false;
+    this._onMediaQueryChange();
+  }
+
+  _handleResize() {
+    clearTimeout(this._resizeDebounce);
+
+    this._resizeDebounce = setTimeout(() => {
+      this._onResize();
+    }, this._resizeDelay);
+  }
+
+  _onMediaQueryChange() {
+    if (this._mediaQueryList.matches) {
+      if (!this._initiated) {
+        this._init();
+      }
+    } else {
+      if (this._initiated) {
+        this._destroy();
+      }
+    }
+  }
+
+  _onResize() {
+    this._viewport.prevWidth = this._viewport.width || window.innerWidth;
+    this._viewport.prevHeight = this._viewport.height || window.innerHeight;
+    this._viewport.width = window.innerWidth;
+    this._viewport.height = window.innerHeight;
+
+    // calculate 100vh
+    const _div = document.createElement('div');
+    _div.style.height = '100vh';
+    document.body.appendChild(_div);
+    this._viewport.height100vh = _div.clientHeight;
+    document.body.removeChild(_div);
+
+    if (this._runResizeHook && this._initiated && this.onResize) {
+      this.onResize(
+        this._viewport,
+        this._viewport.width < 840 &&
+          this._viewport.width === this._viewport.prevWidth &&
+          Math.abs(this._viewport.height - this._viewport.prevHeight) < 120
+      );
     }
 
-    destroy() {
-        if (this.onDestroy) {
-            this.onDestroy();
-        }
-
-        document.documentElement.classList.remove('initiated--' + this._name);
-        this._initiated = false;
-    }
-
-    _bindEvents() {
-        window.addEventListener('resize', (this._handleResize = this._handleResize.bind(this)));
-    }
-
-    _handleResize() {
-        if (this._resizeTimer) {
-            clearTimeout(this._resizeTimer);
-        }
-
-        this._resizeTimer = setTimeout(() => {
-            this._onResize();
-        }, 100);
-    }
-
-    _onResize(runHook = true) {
-        this._viewport.prevWidth = this._viewport.width || window.innerWidth;
-        this._viewport.prevHeight = this._viewport.height || window.innerHeight;
-        this._viewport.width = window.innerWidth;
-        this._viewport.height = window.innerHeight;
-
-        // calculate 100vh
-        const _div = document.createElement('div');
-        _div.style.height = '100vh';
-        document.body.appendChild(_div);
-        this._viewport.height100vh = _div.clientHeight;
-        document.body.removeChild(_div);
-
-        this._checkMediaQuery();
-
-        if (runHook && this._initiated && this.onResize) {
-            this.onResize(
-                this._viewport,
-                this._viewport.width < 800 &&
-                    this._viewport.width === this._viewport.prevWidth &&
-                    Math.abs(this._viewport.height - this._viewport.prevHeight) < 120
-            );
-        }
-    }
-
-    _checkMediaQuery() {
-        if (window.matchMedia(this._mediaQuery).matches) {
-            if (!this._initiated) {
-                this._init();
-            }
-        } else {
-            if (this._initiated) {
-                this.destroy();
-            }
-        }
-    }
+    this._runResizeHook = true;
+  }
 }
